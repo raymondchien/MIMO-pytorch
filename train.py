baf1eeb61b2f8ba66a645e7ce6234126eb451d50
Raymond Chien <rchien.engineer@gmail.com>
2023-05-30 18:21:32 -0400

    initial notes
    added notes to the training and model files.

--- train.py
+++ train.py
@@ -13,19 +13,20 @@ parser.add_argument("--ensemble-num", type=int, default=3)
 
 
 def main(args):
+    # normal configuration stuff
     config = Config(ensemble_num=args.ensemble_num)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    # set up the training data. in this case it is the MNIST dataset
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     train_dataset = datasets.MNIST("../data", train=True, download=True, transform=transform)
     test_dataset = datasets.MNIST("../data", train=False, transform=transform)
-
+    # dataloaders are a datatype that helps accelerate the use of data
     train_dataloaders = [
         DataLoader(train_dataset, batch_size=config.batch_size, num_workers=config.num_workers, shuffle=True)
         for _ in range(config.ensemble_num)
     ]
     test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, num_workers=config.num_workers)
-
+    # Create a MIMO model with config
     model = MIMOModel(ensemble_num=config.ensemble_num).to(device)
     trainer = MIMOTrainer(config, model, train_dataloaders, test_dataloader, device)
     trainer.train()
